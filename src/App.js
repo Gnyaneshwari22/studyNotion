3b15324b00c3e39927b84e9c8b8a785033892c87
Gnyaneshwari22 <gnyaneshwariadki2@gmail.com>
2024-05-30 23:17:41 +0530

    NOMessage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./component/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
